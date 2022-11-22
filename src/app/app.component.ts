a8363110abf6b5f2fa4425c676e491227fbd3125
Madhuveeraprasath <madhu@Madhuveeraprasaths-MacBook-Pro.local>
2022-11-22 18:13:32 +0530

    count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-count';
+  playerCountOne = 0;
+  playerCountTwo = 0;
+  disable = false;
+  add = (player: number) => {
+    if (this.playerCountTwo === 4 || this.playerCountOne === 4) {
+      this.disable = true;
+    }
+    if (player === 1) {
+      this.playerCountOne = this.playerCountOne + 1
+    }
+    if (player === 2) {
+      this.playerCountTwo = this.playerCountTwo + 1
+    }
+  }
+  reset = () => {
+    this.disable = false;
+    this.playerCountOne = 0;
+    this.playerCountTwo = 0;
+  }
 }
+
+//Answer for solution two
+
+// function stringRepeat(a){
+//   myString = a.toLowerCase()
+//    .split("")
+//    .filter((item, index, array) => array.indexOf(item) !== index)
+//    .join("");
+//    return myString;
+// }
+//
+// console.log(stringRepeat('abbsxSX'));
